5dce1551cc2b6eeb7591239c67f1b35652375bfc
oddfredrikhansen <pappafredrik67@hotmail.se>
2016-12-17 14:20:32 +0100

    sql ok

--- app/src/main/java/com/example/fredrik/barometer/MainActivity.java
+++ app/src/main/java/com/example/fredrik/barometer/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.fredrik.barometer;
 
-import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.PopupMenu;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,52 +21,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.text.DateFormat;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import static android.R.color.holo_green_dark;
 
 public class MainActivity extends AppCompatActivity  implements SensorEventListener{
 
     private Sensor mPressure;
     private SensorManager mSensorManager;
     private int previous=0;
-    Integer counter=11;
-    Map<String, Integer> pressures = new HashMap<String, Integer>();
-    MySqlHandler mDbHelper = new MySqlHandler(this); //Instantiera en databas
+    int counter=1;
+    Map<String, Integer> pressures = new HashMap<>();
+    MySqlHandler mDbHelper;//Instantiera en databas
 
     LineGraphSeries<DataPoint> grafData = new LineGraphSeries<>(new DataPoint[] {
-            //new DataPoint(counter, bar),
-            new DataPoint(0, 1013),
-            new DataPoint(1, 1017),
-            new DataPoint(2, 1015),
-            new DataPoint(3, 1019),
-            new DataPoint(4, 1011),
-            new DataPoint(5, 1018),
-            new DataPoint(6, 1019),
-            new DataPoint(7, 1025),
-            new DataPoint(8, 1020),
-            new DataPoint(9, 1023),
-            new DataPoint(10, 1018)
-//            new DataPoint(11, 1025),
-//            new DataPoint(12, 1015),
-//            new DataPoint(13, 1025),
-//            new DataPoint(14, 1027),
-//            new DataPoint(15, 1025),
-//            new DataPoint(16, 1010),
-//            new DataPoint(17, 1025)
+            new DataPoint(0, 1010)
     });
-    //Map<Date, Integer> pressures = new HashMap<Date, Integer>();
 
 
     @Override
@@ -76,6 +48,8 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mDbHelper = new MySqlHandler(this);
+
 
     }
     @Override
@@ -83,29 +57,41 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         // show menu when menu button is pressed
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.my_tab_menu, menu);
-
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // display a message when a button was pressed
-        String message = "";
+        // Gör något när du tryckt
+        String message ;
         if (item.getItemId() == R.id.wtf) {
             message = "A little bit more to the right!";
         }
         else if (item.getItemId() == R.id.cleardatabase) {
             SQLiteDatabase db = mDbHelper.getWritableDatabase();
-            //mDbHelper.getLastIndex(db);
             mDbHelper.onUpgrade(db,1,2);
             message = "Database erased";
+        }
+        else if (item.getItemId() == R.id.history) {
+            message = "Database history";
+            SQLiteDatabase db = mDbHelper.getReadableDatabase();
+            ShowDataHistory.RESULT = mDbHelper.getAll(db);
+            //Startar ett nytt fönster med databasens innehåll
+            Intent intent = new Intent(this, ShowDataHistory.class);
+            startActivity(intent);
         }
         else if (item.getItemId() == R.id.cleargraph) {
             clearGraphValues();
             message = "Graph erased";
         }
         else if (item.getItemId() == R.id.about) {
-            message = "Fredrik Hansen made this";
+
+            message = "Fredrik Hansen made this App";
+        }
+        else if (item.getItemId() == R.id.exit) {
+            message="";
+            finish();
+            System.exit(0);
         }
         else {
             message = "Something went wrong here!?";
@@ -134,16 +120,15 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         if (button.getText().toString().contains("Start")) {
             Log.d("onClick", " Started");
             this.onResume();
-            button.setText("Stop");
-
+            button.setText(R.string.stop);
             button.setBackgroundColor(Color.RED);
         }
 
         else {
             Log.d("onClick", " Stopped");
             this.onPause();
-            button.setText("Start");
-            button.setBackgroundColor(Color.GREEN);
+            button.setText(R.string.start);
+            button.setBackgroundColor(Color.rgb(40,150,20));
         }
     }
 
@@ -163,8 +148,8 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
 
     /**
      * Metod att avrunda decimaltalet till närmsta heltal
-     * @param f
-     * @return
+     * @param f float value in
+     * @return i integer out
      */
     private int round(float f){
         float absNumber = Math.abs(f);
@@ -176,11 +161,12 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
             return f<0 ? -(i+1) : i+1;
         }
     }
+
+
     public void onSensorChanged(SensorEvent event) {
 
         float pressure_value;
-//        float height = 0.0f;
-        int bar=0;
+        int bar;
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm");
         String currentDateandTime = sdf.format(new Date());
 
@@ -200,7 +186,7 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
                 graph.getViewport().setScrollableY(false); // enables vertical scrolling
                 graph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
                 graph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
-
+                graph.getViewport().setMinX(1d);
                 graph.getViewport().setMaxX(20d);
                 graph.setTitle("mBar/hPa");
 
@@ -208,50 +194,55 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
                 graph.addSeries(grafData);
 
                 if(bar<970) {
-                    tv3.setText("Lågtryck, Stormigt");
+                    tv3.setText(R.string.Stormy);
                     tv.setTextColor(Color.BLUE);
+                    iv.setImageDrawable(null);// Sätts så att bilden kan uppdateras
                     iv.setBackgroundResource(R.drawable.storm);
                 }
                 else if(bar<990) {
-                    tv3.setText("Lågtryck, Regn");
+                    tv3.setText(R.string.Rain);
                     tv.setTextColor(Color.LTGRAY);
+                    iv.setImageDrawable(null);// Sätts så att bilden kan uppdateras
                     iv.setBackgroundResource(R.drawable.rain1);
                 }
                 else if(bar<1010) {
-                    tv3.setText("Väderomslag");
+                    tv3.setText(R.string.Change);
                     tv.setTextColor(Color.BLACK);
+                    iv.setImageDrawable(null);// Sätts så att bilden kan uppdateras
                     iv.setBackgroundResource(R.drawable.cloud1);
                 }
                 else if(bar<1030) {
-                    tv3.setText("Högtryck, Stabilt väder");
+                    tv3.setText(R.string.Fair);
                     tv.setTextColor(Color.rgb(40,150,20));
+                    iv.setImageDrawable(null);// Sätts så att bilden kan uppdateras
                     iv.setBackgroundResource(R.drawable.clear1);
                 }
                 else {
-                    tv3.setText("Högtryck, Torka");
+                    tv3.setText(R.string.Draft);
                     tv.setTextColor(Color.RED);
-                    iv.setBackgroundResource(R.drawable.sun1);
+                    iv.setImageDrawable(null);// Sätts så att bilden kan uppdateras
+                    iv.setBackgroundResource(R.drawable.sun1gif);
+                    iv.refreshDrawableState();
                 }
 
                 //Sätt det nya värdet i textview fönstret
                 tv.setText(" "+bar +" ");
                 pressures.put(currentDateandTime,bar);
                 //Spara resultaten till databasen
-                SQLiteDatabase db = mDbHelper.getWritableDatabase();
-                ContentValues values = new ContentValues();
-                values.put(MySqlHandler.ID, counter);
-                values.put(MySqlHandler.DATE, currentDateandTime);
-                values.put(MySqlHandler.MEASUREMENT, bar);
-                // Insert the new row, returning the primary key value of the new row
                 try {
+                    SQLiteDatabase db = mDbHelper.getWritableDatabase();
+                    ContentValues values = new ContentValues();
+                    values.put(MySqlHandler.ID, counter);
+                    values.put(MySqlHandler.DATE, currentDateandTime);
+                    values.put(MySqlHandler.MEASUREMENT, bar);
+                    // Lägger in nya värden i databasen
                     Log.d("Insert rows :", "" + db.insertWithOnConflict(MySqlHandler.TABLENAME, null, values, SQLiteDatabase.CONFLICT_REPLACE));
+                    Log.d("Event match new value :"," bar: "+bar+ ", actual pressure: "+pressure_value+" DB: "+db.getPageSize());
+
                 }catch(Exception e) {Log.e("SQL error: ", "insert fail! "+e);}
 
-
-                Log.d("Event match new value :"," bar: "+bar+ ", actual pressure: "+pressure_value+" DB: "+db.getPageSize());
                 counter++;
             }
-            //height = SensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, pressure_value);
             Log.d("Event :", " bar: " + pressure_value+ "    "+bar+"\t"+ currentDateandTime+" ");
         }
 
--- app/src/main/java/com/example/fredrik/barometer/MySqlHandler.java
+++ app/src/main/java/com/example/fredrik/barometer/MySqlHandler.java
@@ -6,23 +6,29 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
- * Created by fredrik on 2016-12-14.
+ * Created by Fredrik Hansen on 2016-12-10.
+ *
  */
 
 public class MySqlHandler extends SQLiteOpenHelper {
-    public static final String  DBNAME      ="Barran.db";
-    public static final int     VERSION     = 1;
-    public static final String  TABLENAME   ="MYTABLE";
-    public static final String  ID          ="ID";
-    public static final String  DATE        ="DATE";
-    public static final String  MEASUREMENT ="MEASUREMENT";
+    protected static String LAST_DB_VALUE ="";
+    protected static final String  DBNAME      ="Barran.db";
+    protected static final int     VERSION     = 1;
+    protected static final String  TABLENAME   ="MYTABLE";
+    protected static final String  ID          ="ID";
+    protected static final String  DATE        ="DATE";
+    protected static final String  MEASUREMENT ="MEASUREMENT";
 
-    public static final String CREATE_DB ="CREATE TABLE " +TABLENAME + " ("+ID+" ID, "+ DATE + " DATE, " + MEASUREMENT + " MEASUREMENT"+" );";
-    public static final String DELETE_DB ="DROP TABLE IF EXISTS " + TABLENAME;;
-    public static final String LAST_ID = "SELECT ID FROM "+TABLENAME+" ORDER BY ID DESC LIMIT 1";
+    protected static final String CREATE_DB ="CREATE TABLE " +TABLENAME + " ("+ID+" ID, "+ DATE + " DATE, " + MEASUREMENT + " MEASUREMENT"+" );";
+    protected static final String DELETE_DB ="DROP TABLE IF EXISTS " + TABLENAME;
+    private static final String LAST_ID = "SELECT ID FROM "+TABLENAME+" ORDER BY ID DESC LIMIT 1";
+    private static final String GETALL = "SELECT ID FROM "+TABLENAME;
+
 
     public MySqlHandler(Context context){
         super(context, DBNAME, null, VERSION);
@@ -31,6 +37,7 @@ public class MySqlHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         createDB(db);
+        Log.d("PATH for database :",db.getPath());
     }
 
     @Override
@@ -42,18 +49,42 @@ public class MySqlHandler extends SQLiteOpenHelper {
         }catch(Exception e){
             Log.e("Error","Database upgrade failed"+e);}
     }
-    public void getLastIndex(SQLiteDatabase db){
-        Cursor cursor = db.rawQuery(LAST_ID ,null);
-        Log.d("SQL ", "LAST INDEX "+cursor.getColumnCount());
+
+
+    protected String getAll(SQLiteDatabase db){
+        String query = "SELECT * from MYTABLE order by ROWID";
+        String content="";
+        Cursor cursor = db.rawQuery(query,null);
+        int i=0;
+        List<String> fileName = new ArrayList<>();
+        if (cursor.moveToFirst()&& cursor!=null){
+            fileName.add(cursor.getString(cursor.getColumnIndex(ID)));
+            while(cursor.moveToNext()){
+                content = " "+content + "\n "+ " \t" + cursor.getString(cursor.getColumnIndex(DATE))+" \t   "+cursor.getString(cursor.getColumnIndex(MEASUREMENT));
+                //Log.d("getAll ","while: "+" "+cursor.getString(cursor.getColumnIndex(ID))+" "+cursor.getString(cursor.getColumnIndex(DATE))+" "+cursor.getString(cursor.getColumnIndex(MEASUREMENT))+" "+(i++));
+                fileName.add(cursor.getString(cursor.getColumnIndex(DATE)));
+                LAST_DB_VALUE=cursor.getString(cursor.getColumnIndex(MEASUREMENT));
+            }
+        }
+        Log.d("getAll "," Count: "+"# ");
+        return content;
+    }
+    /**
+     * Hämta sista inlagda elementet i databasen
+     * @param db in
+     */
+    public int getLastIndex(SQLiteDatabase db){
+        int lastId=1;
+        String query = "SELECT ROWID from MYTABLE order by ROWID DESC limit 1";
+        Cursor c = db.rawQuery(query,null);
+        if (c != null && c.moveToFirst()) {
+            lastId = c.getInt(0); //Nollan är kolumn index
+        }
+        Log.d("PRINT LAST INDEX ","Count: "+"# "+lastId+" VALUE :"+LAST_DB_VALUE);
+        return lastId;
     }
 
-    public void deleteDatabaseTable(SQLiteDatabase db){
-        Log.d("deleteDatabaseTable","Deletion of database table");
-        try {
-            db.execSQL(DELETE_DB);
-        }catch(Exception e){
-            Log.e("Error","Database deletion failed"+e);}
-    }
+
 
     private void createDB(SQLiteDatabase db){
         db.execSQL(CREATE_DB);
